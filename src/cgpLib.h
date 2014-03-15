deeed7fa3bedb8037d915e7967d40ba2810b508d
Andrew James Turner <turner.andrew.1990@gmail.com>
2014-03-15 17:14:15 +0000

    Update cgpLib.h

--- src/cgpLib.h
+++ src/cgpLib.h
@@ -10,7 +10,7 @@ This file is part of CGP-Library, Andrew James Turner 2014.
     CGP-Library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public License
     along with CGP-Library.  If not, see <http://www.gnu.org/licenses/>.
